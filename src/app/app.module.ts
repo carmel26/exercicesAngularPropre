85c57cb5acd2cc2f61b3d02ec415ef0dd1faba8f
carmel26 <nkeshipelagie@gmail.com>
2021-06-11 14:53:16 +0200

    numero 1 d'avant 14h00 de 14h53 le 11-06-2021

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms'
  
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; 
-import { WarningAlertComponent } from './warning-alert/warning-alert.component';
-import { SuccessAlertComponent } from './success-alert/success-alert.component';
-import { ServerComponent } from './server/server.component';
-import { ServersComponent } from './servers/servers.component';
-
+import { HeaderComponent } from './hearder/header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';  
 @NgModule({
   declarations: [
-    AppComponent, 
-    WarningAlertComponent, SuccessAlertComponent,
-    ServerComponent,ServersComponent
+    AppComponent,  
+    HeaderComponent,
+     RecipesComponent,
+      RecipeListComponent,
+       RecipeDetailComponent,
+        RecipeItemComponent,
+         ShoppingListComponent,
+          ShoppingEditComponent,
   ],
   imports: [
     BrowserModule,
