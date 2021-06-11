bf8205b68bac99167de128fe032af8d2d0fde696
carmel26 <nkeshipelagie@gmail.com>
2021-06-11 17:49:24 +0200

    deuxiem partie de la dataBiding avec un exemple de recipe and book du 11-6-2021 a 17h49

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe('A test recipe','This is for test', 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?webp=true&quality=90&resize=408%2C370'),
+    new Recipe('numero 2','Deuxieme test de recettes', 'https://p7.storage.canalblog.com/79/25/224841/19401251.jpg'),
+  ];
 
   constructor() { }
 
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingrendient } from '../shared/ingrdient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+   ingredients:Ingrendient[] = [
+     new Ingrendient('Apple',5),
+     new Ingrendient('Mangoes',52),
+     new Ingrendient('Tomatos',10),
+   ];
 
   constructor() { }
 
