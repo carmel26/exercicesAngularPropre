6587862f589fc0d9a836454c34dcb01b21378812
carmel26 <nkeshipelagie@gmail.com>
2021-06-17 14:06:57 +0200

    finalisation du lecon 8 et quelques modifications du 17-06-2021 a 14h06

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,8 +8,8 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
-    new Recipe('A test recipe','This is for test', 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?webp=true&quality=90&resize=408%2C370'),
-    new Recipe('numero 2','Deuxieme test de recettes', 'https://p7.storage.canalblog.com/79/25/224841/19401251.jpg'),
+    new Recipe('A test recipe','This is for test', '/assets/images/carmelJunior.jpeg'),
+    new Recipe('numero 2','Deuxieme test de recettes', '/assets/images/princia.jpg'),
   ];
 
  @Output() recipeWasSelected = new EventEmitter<Recipe>();
